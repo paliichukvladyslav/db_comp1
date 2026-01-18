7fb085fbabf1d2fbb7d15a12f5d93935025d667b
Vladyslav <palijczukvladyslav@gmail.com>
2026-01-18 19:00:16 +0200

    [feat/fix] add Windows support to start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,25 @@
 
 VENV_DIR="./venv"
 
+activate_venv() {
+    if [ -f "$VENV_DIR/bin/activate" ]; then
+        # Linux / macOS / WSL
+        source "$VENV_DIR/bin/activate"
+    elif [ -f "$VENV_DIR/Scripts/activate" ]; then
+        # Windows (Git Bash)
+        source "$VENV_DIR/Scripts/activate"
+    else
+        echo "Cannot find venv activate script"
+        exit 1
+    fi
+}
+
 if [ -d $VENV_DIR ]; then
-	source $VENV_DIR/bin/activate
+	activate_venv
 else
 	echo "venv doesn't exist, creating one..."
 	python -m venv venv
-	source $VENV_DIR/bin/activate
+	activate_venv
 	pip install -r requirements.txt
 fi
 
